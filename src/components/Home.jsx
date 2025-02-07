3e62aec8872cad642ada0bcbb7e15b815b9f17e0
AryaN-Alpha <ariyanarshad11@gmail.com>
2025-02-07 23:45:18 +0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -41,7 +41,7 @@ function Home({ selectedCategory }) {
   useEffect(() => {
     const fetchReviews = async () => {
       try {
-        const response = await fetch('server-backend-sigma.vercel.app/reviews');
+        const response = await fetch('server-backend-sable.vercel.app/reviews');
         const data = await response.json(); // Convert to JSON
         setReviews(data.reviews); // Access the reviews directly
       } catch (error) {
