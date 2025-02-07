82766b2bf8a4524ca28b9f8d11831be7196f277a
AryaN-Alpha <ariyanarshad11@gmail.com>
2025-02-07 16:28:03 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
-  base:"/E-commerce-site",
+  plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
-  }
+  },
+  base: process.env.NODE_ENV === "production" ? "/" : "/E-commerce-site/",
 })
